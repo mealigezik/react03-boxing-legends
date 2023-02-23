ba8619a8ca32391aacc2fba144f874aedd12eeb1
Ali Gezik <bilgepanter@gmail.com>
2023-02-23 15:14:47 +0100

    del-warning

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -33,38 +33,3 @@ const CardContainer = ({ name, img, statistics }) => {
   );
 };
 export default CardContainer;
-
-// import Row from "react-bootstrap/Row";
-// import Col from "react-bootstrap/Col";
-// import Form from "react-bootstrap/Form";
-// import { data } from "../helpers/data";
-// import PlayerCard from "./PlayerCard";
-// import { useState } from "react";
-
-// const [search, setSearch] = useState("");
-// console.log(search);
-
-{
-  /* <Form.Control
-        placeholder="Search Player..."
-        type="search"
-        className="w-50 m-auto"
-        onChange={(e) => setSearch(e.target.value)}
-      /> */
-}
-
-{
-  /* <Row className="g-3 justify-content-center">
-          {data
-            .filter((player) =>
-              player.name.toLowerCase().includes(search.trim().toLowerCase())
-            )
-            .map((player, i) => {
-              return (
-                <Col md={6} lg={4} xl={3} key={i}>
-                  <PlayerCard {...player} />
-                </Col>
-              );
-            })}
-        </Row> */
-}
